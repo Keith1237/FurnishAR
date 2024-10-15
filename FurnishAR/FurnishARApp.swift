f6958b40ec50ce109ecc72693afd1410efc950cd
Keith1237 <keithpraveen23@gmail.com>
2024-10-15 18:48:27 +0530

    Initial Commit

---
+++ FurnishAR/FurnishARApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnishARApp.swift
+//  FurnishAR
+//
+//  Created by Keith Praveen on 2024-10-15.
+//
+
+import SwiftUI
+
+@main
+struct FurnishARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FurnishARTests/FurnishARTests.swift
@@ -0,0 +1,17 @@
+//
+//  FurnishARTests.swift
+//  FurnishARTests
+//
+//  Created by Keith Praveen on 2024-10-15.
+//
+
+import Testing
+@testable import FurnishAR
+
+struct FurnishARTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
